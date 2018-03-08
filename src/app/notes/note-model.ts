1fc44d8fcbab471590584c2818f721d5cccdb593
ninagrcia <garcianinalynn@gmail.com>
2018-03-07 17:08:12 -0800

    commits

---
+++ src/app/notes/note-model.ts
@@ -0,0 +1,7 @@
+export interface Note {
+  content: string;
+  hearts: number;
+  dislike: number;
+  id?: string;
+  time: number;
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyDri2CwBtHR12aWFJG_dKLDUW_J_Ey1nLo",
+    authDomain: "angular-ecfff.firebaseapp.com",
+    databaseURL: "https://angular-ecfff.firebaseio.com",
+    projectId: "angular-ecfff",
+    storageBucket: "angular-ecfff.appspot.com",
+    messagingSenderId: "743050459370"
+
+    }
+};
